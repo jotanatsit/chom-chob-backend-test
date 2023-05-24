b13c0f2acc21170d5c45aa2e6c72390d1eefad73
Jo-Tanatsit <jotanatsit@gmail.com>
2023-05-24 14:13:07 +0700

    add url connection string to .sequelizerc

--- database/utils/db.js
+++ database/utils/db.js
@@ -1,18 +1,6 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+import { url } from "../.sequelizerc.js";
 
-/* dotenv.config();
-const dbName = String(process.env.DATABASE_NAME);
-const username = String(process.env.DATABASE_USERNAME);
-const password = String(process.env.DATABASE_PASSWORD);
-
-const url = `postgresql://${username}:${password}@localhost:5432/${dbName}`; */
-
-// * dotenv cannot be read, I don't understand
-
-const sequelize = new Sequelize(
-   "postgresql://postgres:3c9aca00@localhost:5432/chom_chob_test_2",
-   { logging: false }
-);
+const sequelize = new Sequelize(url, { logging: false });
 
 export { sequelize };
